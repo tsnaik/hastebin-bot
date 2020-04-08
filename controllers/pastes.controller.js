2cb1d29cc9d4a0c0f715463fe0dc7118627a251b
Tanmay Naik <tnaik96@gmail.com>
2020-04-07 21:29:38 -0400

    refactor(controller): improved switch case and error handling

--- controllers/pastes.controller.js
+++ controllers/pastes.controller.js
@@ -1,24 +1,35 @@
 const messageService = require('../services/message.service')
 const WELCOME_MESSAGE = 'Welcome to hastebot. Send me a code snippet as a message and I will give you the hastebin URL for that message.';
-module.exports = function (app) {
+module.exports = async function (app) {
     app.get('/hello', (req, res) => res.send("hello world"));
 
-    app.post(`/${process.env.TELEGRAM_TOKEN}/new-message`, function(req, res) {
+    app.post(`/${process.env.TELEGRAM_TOKEN}/new-message`, function (req, res) {
         const { message } = req.body;
         const chatId = message.chat.id;
         console.log(message);
-      
+
         if (!message) {
-          return res.end();
+            return res.end();
         }
-      
+
+        let message;
         switch (message.text) {
             case '/start':
-                messageService.sendMessage(chatId, WELCOME_MESSAGE);
+                message = WELCOME_MESSAGE;
                 break;
             default:
-                messageService.sendMessage(chatId, message.text);
+                message = message.text;
                 break;
         }
-      })
+        
+        try {
+            const response = await messageService.sendMessage(chatId, message);
+            console.log(`Message posted: ${message}`)
+            console.log(response);
+            res.end('ok');
+        } catch (err) {
+            console.log('Error :', err)
+            res.end('Error :' + err)
+        }
+    })
 }
--- services/message.service.js
+++ services/message.service.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-sendMessage = (chatId, text) => {
+sendMessage = (chatId, text) =>
     axios
         .post(
             `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
@@ -9,17 +9,7 @@ sendMessage = (chatId, text) => {
                 text: text
             }
         )
-        .then(response => {
-            // We get here if the message was successfully posted
-            console.log('Message posted')
-            res.end('ok')
-        })
-        .catch(err => {
-            // ...and here if it was not
-            console.log('Error :', err)
-            res.end('Error :' + err)
-        })
-}
+
 
 module.exports = {
     sendMessage
