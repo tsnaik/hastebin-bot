944f9b6c83b17522a4904e7dab0749ec087d14ed
Tanmay Naik <tnaik96@gmail.com>
2020-04-07 21:16:41 -0400

    refactor: moved service to a separate file

--- controllers/pastes.controller.js
+++ controllers/pastes.controller.js
@@ -1,37 +1,23 @@
-const axios = require('axios')
-
+const messageService = require('../services/message.service')
+const WELCOME_MESSAGE = 'Welcome to hastebot. Send me a code snippet as a message and I will give you the hastebin URL for that message.';
 module.exports = function (app) {
     app.get('/hello', (req, res) => res.send("hello world"));
 
     app.post(`/${process.env.TELEGRAM_TOKEN}/new-message`, function(req, res) {
-        const { message } = req.body
+        const { message } = req.body;
         console.log(message);
-        //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
       
-        if (!message || message.text.toLowerCase().indexOf('marco') < 0) {
-          // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-          return res.end()
+        if (!message) {
+          return res.end();
         }
       
-        // If we've gotten this far, it means that we have received a message containing the word "marco".
-        // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-        axios
-          .post(
-            `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
-            {
-              chat_id: message.chat.id,
-              text: 'Polo!!'
-            }
-          )
-          .then(response => {
-            // We get here if the message was successfully posted
-            console.log('Message posted')
-            res.end('ok')
-          })
-          .catch(err => {
-            // ...and here if it was not
-            console.log('Error :', err)
-            res.end('Error :' + err)
-          })
+        switch (message.text) {
+            case '/start':
+                messageService.sendMessage(WELCOME_MESSAGE);
+                break;
+            default:
+                messageService.sendMessage(message.text);
+                break;
+        }
       })
 }
---
+++ services/message.service.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+sendMessage = (text) => {
+    axios
+        .post(
+            `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
+            {
+                chat_id: message.chat.id,
+                text: text
+            }
+        )
+        .then(response => {
+            // We get here if the message was successfully posted
+            console.log('Message posted')
+            res.end('ok')
+        })
+        .catch(err => {
+            // ...and here if it was not
+            console.log('Error :', err)
+            res.end('Error :' + err)
+        })
+}
+
+module.exports = {
+    sendMessage
+}
