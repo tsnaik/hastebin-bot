48549855840bab8cee3bcd0a80635e9098fb3d78
Tanmay Naik <tnaik96@gmail.com>
2020-04-08 00:45:56 -0400

    refactor(cors): changed to cors lib instead of manual

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 var express = require('express')
 var app = express()
+var cors = require('cors')
 var bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000;
 
@@ -9,20 +10,11 @@ app.use(
   bodyParser.urlencoded({
     extended: true
   })
-) // for parsing application/x-www-form-urlencoded
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-        'Content-Type, X-Requested-With, Origin');
-    res.header('Access-Control-Allow-Methods',
-        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
-});
+)
+app.use(cors());
 
 require('./controllers/pastes.controller')(app);
 
-// Finally, start our server
 app.listen(PORT, function() {
   console.log(`Hastebot listening on port ${process.env.PORT}`)
 })
