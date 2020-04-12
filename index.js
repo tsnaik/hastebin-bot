450ab6906390735335392ae8be7e40c0433661db
Tanmay Naik <tnaik96@gmail.com>
2020-04-12 12:53:59 -0400

    fix(index.js): fixed issue in cors
    from e746a838961aca9dbbd4cff92091dd24f2c4c58b

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 var express = require('express')
 var app = express()
-var cors = requirenode('cors')
+var cors = require('cors')
 var bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000;
 
