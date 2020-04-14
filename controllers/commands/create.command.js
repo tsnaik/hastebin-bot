61c4e3f61edddf3a68e0ff4474d197988cb24adc
Tanmay Naik <tnaik96@gmail.com>
2020-04-14 19:55:03 -0400

    feat(setoutputhere): add sender's name in the response when snippet is being sent to different chat
    that way group members will know who created the code snippet

--- controllers/commands/create.command.js
+++ controllers/commands/create.command.js
@@ -6,21 +6,22 @@ module.exports = async (ctx) => {
     const entity = ctx.message.entities.find((value) => value.type === 'bot_command');
 
     if (ctx.message.text.length < entity.length - entity.offset + 1) {
-        text = 'Please add a code snippet after the command'
-    } else {
-        const snippet = ctx.message.text.substring(entity.length - entity.offset + 1);
-        try {
-            const response = await hasteService.createSnippet(snippet);
-            text = `https://www.hastebin.com/${response.data.key}`;
-        } catch (error) {
-            console.error(error.message);
-            text = "Error with the hastebin server. Not my fault!"
-        }
+        return await ctx.reply('Please add a code snippet after the command');
+    }
+    const snippet = ctx.message.text.substring(entity.length - entity.offset + 1);
+    let url = '';
+    try {
+        const response = await hasteService.createSnippet(snippet);
+        url = `https://www.hastebin.com/${response.data.key}`;
+    } catch (error) {
+        console.error(error.message);
+        return await ctx.reply('Error with the hastebin server. Not my fault!');
     }
     const senderId = ctx.from.id.toString();
     if (store.has(senderId)) {
-        return await ctx.telegram.sendMessage(store.get(senderId), text);
-    } else {
-        return await ctx.reply(text);
+        const newText = `[${ctx.from.first_name}](tg://user?id=${senderId}) sent a code snippet:\n\n`
+        .concat(`[Click here to view](${url})`);
+        return await ctx.telegram.sendMessage(store.get(senderId), newText, { parse_mode: 'MarkdownV2' });
     }
+    return await ctx.reply(url);
 }
